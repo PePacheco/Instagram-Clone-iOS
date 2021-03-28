fcb22d14bd16242b2a767e6e8f70769d80e8a7f4
PePacheco <pedro_pacheco98@outlook.com>
2021-03-27 22:23:50 -0300

    Initial Commit

---
+++ Instagram-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 27/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
